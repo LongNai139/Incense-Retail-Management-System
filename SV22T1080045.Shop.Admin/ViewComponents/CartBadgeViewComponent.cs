646b23f4465b0cfd2bcc84d441e78e1b8c53c026
Mai Bao Long <maibaolong0913@gmail.com>
2026-05-30 17:49:39 +0700

    refactor(nav): simplify header layout and cart badge component

---
+++ SV22T1080045.Shop.Admin/ViewComponents/CartBadgeViewComponent.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using SV22T1080045.Shop.BusinessLayers.Interfaces;
+
+namespace SV22T1080045.Shop.Admin.ViewComponents
+{
+    public class CartBadgeViewComponent : ViewComponent
+    {
+        private readonly ICartService _cartService;
+
+        public CartBadgeViewComponent(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_cartService.Count());
+        }
+    }
+}
